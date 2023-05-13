04aaaaa269d31f801aec93c5638181093eeeac67
radwija <115165737+radwija@users.noreply.github.com>
2023-05-13 20:39:03 +0800

    fixing bidding

--- src/main/java/com/abccarportal/controller/CarController.java
+++ src/main/java/com/abccarportal/controller/CarController.java
@@ -84,21 +84,21 @@ public class CarController {
 
 		Car cardata = car_info.get();
 
-		List<Car> car = new ArrayList<Car>();
+		List<Car> carList = new ArrayList<Car>();
 
-		car.add(cardata);
+		carList.add(cardata);
 
-		model.addAttribute("car", car);
+		model.addAttribute("carList", carList);
 
 		// show bidding data
 		Predicate<CarBidding> byCar = ucar -> ucar.getCar().equals(cardata);
 
-		List<CarBidding> bdata = carService.getAllBids().stream().filter(byCar).collect(Collectors.toList());
+		List<CarBidding> bdataList = carService.getAllBids().stream().filter(byCar).collect(Collectors.toList());
 
-		model.addAttribute("bid", bdata);
+		model.addAttribute("bidList", bdataList);
 
-		if (bdata.size() > 0) {
-			double highest = bdata.get(0).getBidderPrice();
+		if (bdataList.size() > 0) {
+			double highest = bdataList.get(0).getBidderPrice();
 			System.out.println(highest);
 			model.addAttribute("highest", highest);
 		}
@@ -160,73 +160,74 @@ public class CarController {
 		User user = userService.findLoginUser(uname);
 
 		// get Car
-		Optional<Car> car_info = carService.getCarInfo(id);
-		System.out.println(car_info);
+		Car carInfo = carService.getCarInfo(id).orElse(null);
+		System.out.println(carInfo);
 
-		Car cardata = car_info.get();
+		if (carInfo != null) {
+			Car cardata = carInfo;
 
-		bid.setBidderName(user.getName());
-		bid.setUser(user);
-		bid.setCar(cardata);
+			bid.setBidderName(user.getName());
+			bid.setUser(user);
+			bid.setCar(cardata);
 
-		// before saving, check if user has placed a bid before
-		List<CarBidding> userBid = carService.findUserBid(user, cardata);
-		System.out.println(userBid);
+			// before saving, check if user has placed a bid before
+			List<CarBidding> userBid = carService.findUserBid(user, cardata);
+			System.out.println(userBid);
 
-		if (userBid.size() > 0) {
+			if (userBid.size() > 0) {
 
-			long bidId = userBid.stream().findAny().get().getId();
-			Optional<CarBidding> bid_info = carService.getBidInfo(bidId);
-			System.out.println(bid_info);
+				long bidId = userBid.stream().findAny().get().getId();
+				Optional<CarBidding> bid_info = carService.getBidInfo(bidId);
+				System.out.println(bid_info);
 
-			CarBidding bidData = bid_info.get();
+				CarBidding bidData = bid_info.get();
 
-			bidData.setBidderPrice(price);
+				bidData.setBidderPrice(price);
 
-			carService.saveBid(bidData);
-			
-			
-		} else {
-			carService.saveBid(bid);
-			
+				carService.saveBid(bidData);
+
+			} else {
+				carService.saveBid(bid);
+
+			}
+
+			// show car data
+
+			List<Car> car = new ArrayList<Car>();
+
+			car.add(cardata);
+
+			model.addAttribute("car", car);
+
+			// show bidding data
+			Predicate<CarBidding> byCar = ucar -> ucar.getCar().equals(cardata);
+
+			List<CarBidding> bdata = carService.getAllBids().stream().filter(byCar).collect(Collectors.toList());
+			System.out.println(bdata);
+
+			model.addAttribute("bid", bdata);
+
+			if (bdata.size() > 0) {
+				double highest = bdata.get(0).getBidderPrice();
+				System.out.println(highest);
+				model.addAttribute("highest", highest);
+			}
+
+			Set<CarBidding> highBid = new HashSet<CarBidding>();
+			highBid.add(bdata.get(0));
+
+			// cardata.getBiddings().clear();
+			cardata.setBiddings(highBid);
+			carService.save(cardata);
+			System.out.println(cardata.getBiddings());
+
 		}
-
-		// show car data
-
-		List<Car> car = new ArrayList<Car>();
-
-		car.add(cardata);
-
-		model.addAttribute("car", car);
-
-		// show bidding data
-		Predicate<CarBidding> byCar = ucar -> ucar.getCar().equals(cardata);
-
-		List<CarBidding> bdata = carService.getAllBids().stream().filter(byCar).collect(Collectors.toList());
-		System.out.println(bdata);
-
-		model.addAttribute("bid", bdata);
-
-		if (bdata.size() > 0) {
-			double highest = bdata.get(0).getBidderPrice();
-			System.out.println(highest);
-			model.addAttribute("highest", highest);
-		}
-		
-		Set<CarBidding> highBid = new HashSet<CarBidding>();
-		highBid.add(bdata.get(0));
-		
-		//cardata.getBiddings().clear();
-		cardata.setBiddings(highBid);
-		carService.save(cardata);
-		System.out.println(cardata.getBiddings());
-
 		return "carDetail";
 	}
 
 	@PostMapping("book")
 	public String carBooking(@ModelAttribute("booking") CarBidding book,
-			@RequestParam(value = "cid", required = false) long id, Principal principal, 
+			@RequestParam(value = "cid", required = false) long id, Principal principal,
 			Model model, @RequestParam("bookDate") String date) {
 
 		// Get User name
@@ -255,12 +256,12 @@ public class CarController {
 			System.out.println(book_info);
 
 			CarBidding bookdata = book_info.get();
-			
+
 			bookdata.setBookDate(date);
 
 			carService.saveBid(bookdata);
 		} else {
-			
+
 			carService.saveBid(book);
 		}
 
@@ -285,108 +286,107 @@ public class CarController {
 			System.out.println(highest);
 			model.addAttribute("highest", highest);
 		}
-		
+
 		Set<CarBidding> highBid = new HashSet<CarBidding>();
 		highBid.add(bdata.get(0));
-		
-		//cardata.getBiddings().clear();
+
+		// cardata.getBiddings().clear();
 		cardata.setBiddings(highBid);
 		carService.save(cardata);
 		System.out.println(cardata.getBiddings());
-		
 
 		return "carDetail";
 	}
-	
+
 	@GetMapping("deletecar")
 	public String deleteCar(@RequestParam long cid) {
-		
+
 		carService.deleteCar(cid);
-		
+
 		return "redirect:cars";
-		
+
 	}
-	
+
 	@GetMapping("all_cars")
 	public ModelAndView showAllCar(@ModelAttribute("car") Car car) {
-		
+
 		List<Car> cars = carService.getAllCars();
 		System.out.println(cars);
-		
-		return new ModelAndView ("allCar", "cars", cars);
+
+		return new ModelAndView("allCar", "cars", cars);
 	}
-	
+
 	@PostMapping("edit_car")
-	public String editCar(@ModelAttribute("car") Car car, 
+	public String editCar(@ModelAttribute("car") Car car,
 			@RequestParam long cid) {
-		
+
 		Optional<Car> car_info = carService.getCarInfo(cid);
-    	System.out.println(car_info);
-    	
-    	Car edit_car = car_info.get();
-		
-    	edit_car.setMake(car.getMake());
-    	edit_car.setModel(car.getModel());
-    	edit_car.setName(car.getName());
-    	edit_car.setPrice(car.getPrice());
-    	edit_car.setRegisteration(car.getRegisteration());
-    	edit_car.setSellerId(car.getSellerId());
-    	edit_car.setPhotos(car.getPhotos());
-    	edit_car.setPhotoImagePath(car.getPhotoImagePath());
-    	
-    	carService.save(edit_car);
-		
+		System.out.println(car_info);
+
+		Car edit_car = car_info.get();
+
+		edit_car.setMake(car.getMake());
+		edit_car.setModel(car.getModel());
+		edit_car.setName(car.getName());
+		edit_car.setPrice(car.getPrice());
+		edit_car.setRegisteration(car.getRegisteration());
+		edit_car.setSellerId(car.getSellerId());
+		edit_car.setPhotos(car.getPhotos());
+		edit_car.setPhotoImagePath(car.getPhotoImagePath());
+
+		carService.save(edit_car);
+
 		return "redirect:all_cars";
 	}
-	
+
 	@GetMapping("delete_car")
 	public String deletePostCar(@RequestParam long cid) {
-		
+
 		carService.deleteCar(cid);
-		
+
 		return "redirect:all_cars";
-		
+
 	}
-	
+
 	@PostMapping("booking_status")
 	public String setCarBookingStatus(@RequestParam long bid,
 			@ModelAttribute("book") CarBidding book) {
-		
+
 		Optional<CarBidding> car_info = carService.getBidInfo(bid);
 		CarBidding car_booking = car_info.get();
 		System.out.println(car_booking);
-		
+
 		car_booking.setBookStatus(book.getBookStatus());
-		
+
 		carService.saveBid(car_booking);
-		
+
 		return "redirect:all_cars";
 	}
-	
+
 	@PostMapping("sale_status")
 	public String setCarSaleStatus(@RequestParam long cid,
 			@ModelAttribute("car") Car car) {
-		
+
 		Optional<Car> car_info = carService.getCarInfo(cid);
 		Car car_sale = car_info.get();
 		System.out.println(car_sale);
-		
+
 		car_sale.setSellStatus(car.getSellStatus());
-		
+
 		carService.save(car_sale);
-		
+
 		return "redirect:all_cars";
 	}
-	
+
 	@GetMapping("search")
 	public ModelAndView searchCars(@RequestParam String keyword, Model model) {
-		
+
 		List<Car> searchCar = carService.search(keyword);
 		System.out.println(searchCar);
-		
+
 		model.addAttribute("keyword", keyword);
-		
-		return new ModelAndView ("searchResults", "searchCar", searchCar);
+
+		return new ModelAndView("searchResults", "searchCar", searchCar);
 	}
 
 }
